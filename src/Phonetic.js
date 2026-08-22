af0bfe569f4c74ff04a7f0532e6ebe7340053af9
Gabcii <gderzsi@yahoo.co.uk>
2026-08-22 18:22:09 +0100

    added audio button

---
+++ src/Phonetic.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  console.log(Phonetic);
+  return <div className="Phonetic"></div>;
+}
--- src/Results.js
+++ src/Results.js
@@ -1,18 +1,29 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
+  function playAudio() {
+    let utterance = new SpeechSynthesisUtterance(props.result.word);
+    window.speechSynthesis.speak(utterance);
+  }
+  console.log(props.data);
   if (props.result) {
     return (
       <div className="Results">
         <h2>{props.result.word}</h2>
-        {props.result.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
-          );
-        })}
+        <button onClick={playAudio}>🔊 Listen</button>
+        {props.result.phonetic && (
+          <div className="Phonetic">{props.result.phonetic}</div>
+        )}
+        {props.result.meanings &&
+          props.result.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}
       </div>
     );
   } else {
