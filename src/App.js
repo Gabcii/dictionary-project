0a2c8be18bc04cbf6d8c5362ce78d1af665123c2
Gabcii <gderzsi@yahoo.co.uk>
2026-08-22 13:47:09 +0100

    set up dictionary component and added state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import mylogo from "./mylogo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={mylogo} alt="mylogo" className="Dictionary-logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={mylogo} alt="mylogo" className="Dictionary-logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          Coded by Gabriella Derzsi, open-sourced on{" "}
+          <a href="https://github.com/Gabcii/dictionary-project">Github </a>and
+          hosted on <a href="https://dictionary-app-gd.netlify.app">Netlify.</a>
+        </footer>
+      </div>
     </div>
   );
 }
